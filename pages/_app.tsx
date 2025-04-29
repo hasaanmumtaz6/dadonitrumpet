b7356eb8c55e91516a7f31fe1116ed5eeb278ddb
Muhammad Hasaan Mumtaz <130135831+hasaanmumtaz6@users.noreply.github.com>
2025-04-29 08:20:26 +0000

    FOOTER CDE CHANGED

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import "@/styles/Header.css";
 import "@/styles/Intro.css";
+import "@/styles/Footer.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
