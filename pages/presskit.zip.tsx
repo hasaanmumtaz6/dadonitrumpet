156d3b6833ef1240f54f45b2b902fd10a2da7d71
hasaanmumtaz6 <hasaanmumtaz6@gmail.com>
2025-05-27 00:25:11 +0500

    few changes in the presskit.zip page

--- pages/presskit.zip.tsx
+++ pages/presskit.zip.tsx
@@ -5,8 +5,6 @@ import { FaFileZipper } from "react-icons/fa6";
 const Presskit = () => {
   return (
     <div className="presskit-zip-page-container">
-      <h2>Presskit.zip...</h2>
-      <p>Under Developemnt</p>
       <Link href="/docs/presskit.zip" download="Presskit" className="primary-btn">
         <FaFileZipper />
         <b>Dowload Zip File</b>
