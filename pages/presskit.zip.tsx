7d121a19721d30b881e0c2f2cb069bf12a229b2b
hasaanmumtaz6 <hasaanmumtaz6@gmail.com>
2025-05-27 00:34:13 +0500

    file downloading automaticly has been added on the press kit page

--- pages/presskit.zip.tsx
+++ pages/presskit.zip.tsx
@@ -1,11 +1,27 @@
+import Head from "next/head";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { FaFileZipper } from "react-icons/fa6";
 
 const Presskit = () => {
+  useEffect(() => {
+    const link = document.createElement("a");
+    link.href = "/docs/presskit.zip";
+    link.setAttribute("download", "presskit.zip");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }, []);
   return (
     <div className="presskit-zip-page-container">
-      <Link href="/docs/presskit.zip" download="Presskit" className="primary-btn">
+      <Head>
+        <title>Presskit.zip</title>
+      </Head>
+      <Link
+        href="/docs/presskit.zip"
+        download="Presskit"
+        className="primary-btn"
+      >
         <FaFileZipper />
         <b>Dowload Zip File</b>
       </Link>
