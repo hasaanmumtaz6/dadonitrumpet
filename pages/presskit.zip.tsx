1ee5785ccfe0642d72d36a8079e6b134b044fedc
hasaanmumtaz6 <hasaanmumtaz6@gmail.com>
2025-05-27 23:36:03 +0500

    unusual comment removed from the code and no use import code removed from the presskit.zip page

--- pages/presskit.zip.tsx
+++ pages/presskit.zip.tsx
@@ -1,11 +1,7 @@
 import Head from "next/head";
-import Link from "next/link";
-import React, { useEffect, useRef, useState } from "react";
-import { FaFileZipper } from "react-icons/fa6";
+import React, { useEffect, useRef } from "react";
 
 const Presskit = () => {
-  // const [zipNoti, setZipNoti] = useState("");
-  // const [buttonText, setButtonText] = useState("Downloading Press Kit");
   const hasRun = useRef(false);
 
   useEffect(() => {
@@ -21,45 +17,13 @@ const Presskit = () => {
     document.body.appendChild(link);
     link.click();
     link.remove();
-
-    // setZipNoti("presskit.zip downloaded");
-    // setButtonText("Downloaded ✅");
-
-    // const timer = setTimeout(() => {
-    //   setZipNoti("");
-    //   // setButtonText("Downloading Press Kit");
-    // }, 15000);
-
-    // return () => clearTimeout(timer);
   }, []);
-
-  // const handleClick = () => {
-  //   setZipNoti("presskit.zip downloaded");
-  //   setButtonText("Downloaded ✅");
-
-  //   setTimeout(() => {
-  //     setButtonText("Downloading Press Kit");
-  //   }, 10000);
-  //   setTimeout(() => {
-  //     setZipNoti("");
-  //   }, 8000);
-  // };
 
   return (
     <div className="presskit-zip-page-container">
       <Head>
         <title>Presskit.zip</title>
       </Head>
-      {/* <Link
-        href="/docs/presskit.zip"
-        download="Presskit"
-        className="primary-btn"
-        onClick={handleClick}
-      >
-        <FaFileZipper />
-        <b>{buttonText}</b>
-      </Link>
-      {zipNoti && <p className="animate-pulse select-none">{zipNoti}</p>} */}
     </div>
   );
 };
